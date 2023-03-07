b35d624947626d4c284c316665afaf382e6156f2
KateSebring <ihatesuperman99@gmail.com>
2023-03-07 13:25:32 -0500

    Create index.php
    Created index.php for home page.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+ // this will be for future features
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>One Fell Soup</title>
+</head>
+<body>
+    <h1>One Fell Soup</h1>
+
+    <p>This is a test page.</p>
+</body>
+</html>
